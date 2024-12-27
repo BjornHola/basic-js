409f1ed6e340e5d5d8389d1c42270d575addfb67
BjornHola <broadherstby@gmail.com>
2024-12-27 23:30:01 +0300

    init: filfil task about creating Dreamteam name like abbreviation taken from array

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let secretName = members
+    .filter(element => typeof element === 'string')
+    .map(element => element.trim().charAt(0).toUpperCase())
+    .sort()
+    .join('');
+  return secretName;
 }
+console.log(createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']));
+console.log(createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]));
 
 module.exports = {
   createDreamTeam
