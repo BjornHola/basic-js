59e53f0cf99be638bc937f183116f04655e866df
BjornHola <broadherstby@gmail.com>
2024-12-27 18:55:13 +0300

    init: fulfil task with count cats at backyard

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let count = 0;
+  for (let row of matrix){
+    for (let data of row){
+      if (data === '^^'){
+      count ++;
+      }
+    }
+  }
+  return count;
 }
+console.log(countCats([[0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2]]));
 
 module.exports = {
   countCats
