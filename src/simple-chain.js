24831d2ddc94c9c14efc62ecf036478cf163b2d8
BjornHola <broadherstby@gmail.com>
2024-12-31 21:16:12 +0300

    fix: fix message with error, delete comments

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -17,7 +17,7 @@ const chainMaker = {
   removeLink(position) {
     if (!Number.isInteger(position) || Number.isNaN(position) || position < 0 || position > this.chain.length){
       this.chain = [];
-      throw new Error ('You can\'t remove incorrect link!');
+      throw new Error ("You can't remove incorrect link!");
     } 
     this.chain.splice(position - 1, 1)
       return this;
@@ -33,9 +33,9 @@ const chainMaker = {
       }
 }
 
-console.log(chainMaker.addLink(1).addLink(2).addLink(3).finishChain()); // '( 1 )~~( 2 )~~( 3 )'
-console.log(chainMaker.addLink(1).addLink(2).removeLink(1).addLink(3).finishChain()); // '( 2 )~~( 3 )'
-console.log(chainMaker.addLink(1).addLink(2).reverseChain().addLink(3).finishChain()); // '( 2 )~~( 1 )~~( 3 )'
+console.log(chainMaker.addLink(1).addLink(2).addLink(3).finishChain()); 
+console.log(chainMaker.addLink(1).addLink(2).removeLink(1).addLink(3).finishChain()); 
+console.log(chainMaker.addLink(1).addLink(2).reverseChain().addLink(3).finishChain()); 
 
 module.exports = {
   chainMaker
