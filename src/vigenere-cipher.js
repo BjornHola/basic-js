88d680f1b6ee2ad42bcc070ae76f2e06f84d7789
BjornHola <broadherstby@gmail.com>
2025-01-02 16:21:55 +0300

    init:fulfil  task cipher of Vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i];
+      if (char >= 'A' && char <= 'Z') {
+        const m = char.charCodeAt(0) - 65;
+        const k = key[keyIndex % key.length].charCodeAt(0) - 65;
+        const c = (m + k) % 26;
+        result += String.fromCharCode(c + 65);
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const char = encryptedMessage[i];
+      if (char >= 'A' && char <= 'Z') {
+        const c = char.charCodeAt(0) - 65;
+        const k = key[keyIndex % key.length].charCodeAt(0) - 65;
+        const m = (c - k + 26) % 26;
+        result += String.fromCharCode(m + 65);
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
+
+
+const directMachine = new VigenereCipheringMachine();
+const reverseMachine = new VigenereCipheringMachine(false);
+
+console.log(directMachine.encrypt('attack at dawn!', 'alphonse')); // 'AEIHQX SX DLLU!'
+console.log(directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse')); // 'ATTACK AT DAWN!'
+
+console.log(reverseMachine.encrypt('attack at dawn!', 'alphonse')); // '!ULLD XS XQHIEA'
+console.log(reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse')); // '!NWAD TA KCATTA'
+
 
 module.exports = {
   VigenereCipheringMachine
