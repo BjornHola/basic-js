fd2c987de7d3a823e897ca53059eb9ea95a01d46
BjornHola <broadherstby@gmail.com>
2025-10-31 13:01:55 +0300

    init: task cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,9 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  const filtered =  matrix.flat().filter(item => item === '^^' );
+  return filtered.length;
 }
 
 module.exports = {
